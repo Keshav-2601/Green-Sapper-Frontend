0c7d1d6714ceef9d57a35942b462fa2d9190d68a
Keshav-2601 <keshavv857@gmail.com>
2024-08-02 15:51:28 +0100

    git 124

--- functions/contact.js
+++ functions/contact.js
@@ -1,31 +1,30 @@
-import { MongoClient } from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 exports.handler = async function(event, context) {
-    console.log("event data is ",event);
-  const client = new MongoClient(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+    const client = new MongoClient(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
-  try {
-    await client.connect();
-    const db = client.db();
-    const contacts = db.collection('Contact_Table');
+    try {
+        await client.connect();
+        const db = client.db('greensapper');
+        const contacts = db.collection('contacts');
 
-    const { name, email, message } = JSON.parse(event.body);
+        const { name, email, message } = JSON.parse(event.body);
 
-    await contacts.insertOne({ name, email, message });
+        await contacts.insertOne({ name, email, message });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Contact saved successfully' })
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Internal Server Error', error: error.message })
-    };
-  } finally {
-    await client.close();
-  }
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Contact saved successfully' })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error', error: error.message })
+        };
+    } finally {
+        await client.close();
+    }
 };
