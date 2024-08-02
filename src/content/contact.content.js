465d4eea138f7132cbf15a206431c78ff7324b4a
Keshav-2601 <keshavv857@gmail.com>
2024-08-02 15:11:54 +0100

    git again

--- src/content/contact.content.js
+++ src/content/contact.content.js
@@ -21,7 +21,7 @@ export default class Contactcontent extends React.Component {
         console.log("Attempting to submit:", this.state);
         try {
             console.log("Sending Axios request");
-            const response = await Axios.post(`${process.env.REACT_APP_BACKEND_API}/Contacts`, {
+            const response = await Axios.post(`${process.env.REACT_APP_BACKEND_API}`, {
                 name: this.state.name,
                 email: this.state.email,
                 message: this.state.message
