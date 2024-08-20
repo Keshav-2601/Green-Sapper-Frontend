a831924e74dd37838e5b8526c443285256c64ff9
Keshav-2601 <keshavv857@gmail.com>
2024-08-20 12:30:55 +0100

    git new api

--- api/contact.js
+++ api/contact.js
@@ -4,22 +4,20 @@ import { mongodbconnection } from '../config/monggodb.js';
 const contactControl = new contactcontroller();
 
 export default async function handler(req, res) {
-    // Ensure the database connection is established before handling requests
-    if (!global.mongoClient) {
+    try {
+        // Ensure the database connection is established
         await mongodbconnection();
-    }
 
-    // Check the request method
-    if (req.method === 'POST') {
-        try {
+        // Check the request method
+        if (req.method === 'POST') {
             console.log('Received request:', req.body);
             await contactControl.get(req, res);
-        } catch (error) {
-            console.log("Error in POST /api/contact:", error);
-            res.status(500).send('Internal Server Error');
+        } else {
+            res.setHeader('Allow', ['POST']);
+            res.status(405).end(`Method ${req.method} Not Allowed`);
         }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+    } catch (error) {
+        console.error("Error in POST /api/contact:", error);
+        res.status(500).json({ error: { code: '500', message: 'A server error has occurred' } });
     }
 }
