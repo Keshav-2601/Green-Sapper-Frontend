0e82330bcfb33ec1bb3d1fb2fe7e776be9b7d1f7
Keshav-2601 <keshavv857@gmail.com>
2024-08-21 11:50:21 +0100

    delete the backedn path

--- src/pages/api/contactRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import ContactController from './contactController.js';
-const contactrouter=express.Router();
-const contactcontroller=new ContactController();
-contactrouter.post('/',(res,req)=>{
-  contactcontroller.getcontact(res,req);
-})
-
-export{contactrouter};
